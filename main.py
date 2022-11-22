8c8c3e6f0f30f809ce712c2eea49e1364488e73c
Christian Daniel Alameda <115608055+ChristianAlameda@users.noreply.github.com>
2022-11-22 12:41:51 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from mainfunctions import *
 from interface import *
 # The main role of this project is to store and output files
-# It would output the meals, type of meal it is, ingridient,
+# It would output the meals, type of meal it is, ingredient,
 # price, and calories for the user
 
-# Yumong Lee 15-16
+
 if __name__ == "__main__":
     Interface()
